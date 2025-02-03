b57c43c38569b4d6b8e8cbf3e204e9f5b4201671
Malaika Poluscar <polusmal001@warren.k12.in.us>
2025-02-03 08:28:49 -0500

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,13 @@
+const book = {
+    title: "The Poet X",
+    author: "Elizabeth Acevedo",
+    year: "2018",
+    getSummary: function (){
+        return `Title: ${this.title}`;  // TODO: continue with remaining properties of the book (author, year)
+
+    }
+};
+
+// TODO: add a genre key value pair.
+// TODO: delete the year property
+// TODO: console.log(book)
